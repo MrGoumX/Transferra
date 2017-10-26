3a1a28df11e52244c50d2cc26a7fceb3d2b6d1d1
Konstantinos <koslamprakis@gmail.com>
2017-10-26 13:23:42 +0300

    Commit

--- src/sample/ReceiveWindows.java
+++ src/sample/ReceiveWindows.java
@@ -1,4 +1,19 @@
 package sample;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public class ReceiveWindows {
+
+    public void initialize(){
+        try{
+            URL whatismyip = new URL("http://checkip.amazonaws.com");
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    whatismyip.openStream()));
+
+            String ip = in.readLine(); //you get the IP as a String
+           //ipTextField.setText(ip);
+        }catch (Exception e){}
+    }
 }
